de09bbb34c1f6daa3e9d02328a980dffc98cb52d
Ouvill <contact@ouvill.net>
2024-08-13 14:16:23 +0900

    chore: Add vite-plugin-static-copy dev dependency
    This commit includes the addition of the `vite-plugin-static-copy` package to the devDependencies in the package.json and package-lock.json files. This plugin will assist in copying static assets during the build process.

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,45 @@
 // vite.config.ts
 
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "README.md",
+          dest: "",
+        },
+        {
+          src: "README.md",
+          dest: "",
+          rename: "README.txt",
+        },
+        {
+          src: "LICENSE",
+          dest: "",
+        },
+      ],
+    }),
+  ],
   build: {
     minify: false,
     lib: {
-      entry: 'src/main.ts',
-      name: 'TyranoVoiceVoxPlugin',
-      fileName: (format) => `tyrano-voicevox-plugin.${format}.js`
+      entry: "src/main.ts",
+      name: "TyranoVoiceVoxPlugin",
+      fileName: (format) => `tyrano-voicevox-plugin.${format}.js`,
     },
     rollupOptions: {
       output: {
         compact: true,
         generatedCode: {
-          symbols: false
+          symbols: false,
         },
         globals: {
-          TYRANO: 'TYRANO'
-        }
-      }
-    }
-  }
+          TYRANO: "TYRANO",
+        },
+      },
+    },
+  },
 });
