f14403cecf86864d76b301e8b0aa36ca19ca967c
Ouvill <contact@ouvill.net>
2024-08-17 00:45:46 +0900

    svelteのuiをオンオフするように変更

--- src/ui/Hello.svelte
+++ src/ui/Hello.svelte
@@ -1,26 +1,36 @@
 <script lang="ts">
-
-
+    import {isDevOpen} from "./store.ts"
+    import {fade, fly} from "svelte/transition";
 </script>
 
-<div class="container">
-
-    <p class="content">hello world from svelte</p>
-</div>
+{#if $isDevOpen}
+    <!-- svelte-ignore -->
+    <div class="container" transition:fly={{y: 200, duration: 300}}
+         on:click={(e) => {e.stopPropagation()}}
+    >
+        <div in:fade={{duration: 300, delay: 300}}>
+            <h1>VOICEVOX管理UI</h1>
+            <p class="content">hello world from svelte</p>
+        </div>
+    </div>
+{/if}
 
 <style>
     .container {
         position: fixed;
+        display: flex;
+        justify-content: center;
         top: 0;
         left: 0;
         right: 0;
         bottom: 0;
         color: white;
         background: lightskyblue;
+        pointer-events: auto;
+        z-index: 999999998;
     }
 
     .content {
-        text-align: right;
 
     }
 </style>
--- src/ui/addDevUi.ts
+++ src/ui/addDevUi.ts
@@ -1,10 +1,31 @@
 import App from "./Hello.svelte";
+import { isDevOpen } from "./store.ts";
+import { get } from "svelte/store";
 
 export function addDevUi() {
   console.log("adding");
-  const div = document.createElement("div");
-  div.id = "svelte";
 
+  const reloadButtonContainer = document.querySelector(
+    "body > .ui-draggable-handle.ui-draggable",
+  );
+
+  const button = document.createElement("button");
+  button.innerText = "VoiceVoxUI 開く";
+  button.style.marginLeft = "4px";
+  button.style.marginLeft = "4px";
+  button.onclick = (e) => {
+    e.stopPropagation()
+    isDevOpen.update((bool) => !bool);
+    button.innerText = `VoiceVoxUI ${get(isDevOpen) ? "閉じる" : "開く"}`;
+  };
+
+  if (reloadButtonContainer !== null) {
+    reloadButtonContainer.appendChild(button);
+  }
+
+  const div = document.createElement("div");
+
+  div.id = "svelte";
 
   document.body.appendChild(div);
   return new App({
