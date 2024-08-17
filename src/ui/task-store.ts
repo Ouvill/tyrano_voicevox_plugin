b9ea2198841bad2c00d58987891e25e84a30a9c4
Ouvill <contact@ouvill.net>
2024-08-18 02:41:16 +0900

    fix: hash

--- src/ui/task-store.ts
+++ src/ui/task-store.ts
@@ -2,7 +2,7 @@ import { SpeechTask } from "../models/speech-task.ts";
 import { writable } from "svelte/store";
 import { produce } from "immer";
 
-type TaskId = number;
+type TaskId = string;
 
 type TaskMemoryStore = {
   data: Record<TaskId, SpeechTask>;
@@ -14,7 +14,7 @@ const initialData: TaskMemoryStore = { data: {}, order: [] };
 function djb2Hash(str: string) {
   let hash = 5381;
   for (let i = 0; i < str.length; i++) {
-    hash = (hash << (5 + hash)) + str.charCodeAt(i);
+    hash = (hash << 5) + hash + str.charCodeAt(i);
     hash = hash & 0xffffffff;
   }
   return hash >>> 0;
@@ -25,11 +25,14 @@ function createTaskStore() {
 
   function generateTaskId(task: SpeechTask): TaskId {
     const taskString = JSON.stringify(task);
-    return djb2Hash(taskString);
+    const hash = djb2Hash(taskString);
+    console.log("generated hash: ", hash);
+    return hash.toString(16);
   }
 
   function addTask(task: SpeechTask) {
     const taskId = generateTaskId(task);
+    console.log("generated taskId:", taskId);
     update((state) =>
       produce(state, (draft) => {
         if (!draft.data[taskId]) {
